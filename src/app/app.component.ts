0c4b356e1bae53d8c19b6da85271a16ad28a17f1
alkavats1 <alka.vats@knoldus.com>
2022-07-20 13:43:46 +0530

    Weather_App_in_Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { WeatherService } from './services/weather.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  cityName:string="Asansol";
+  weatherData:WeatherData;
+
+  constructor(private weatherService: WeatherService){}
+
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+    this.cityName="";
+  }
+
+  onSubmit(){
+    this.getWeatherData(this.cityName);
+    this.cityName="";
+  }
+
+  private getWeatherData(cityName:string){
+    this.weatherService.getWeatherData(cityName)
+      .subscribe({
+        next:(response) => {
+          this.weatherData=response;
+          console.log(response);
+        }
+      });
+  }
+  
+}
